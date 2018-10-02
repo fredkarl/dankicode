e14a48b24067a6a276ac9040a88770355d6ec16e
= <=>
2018-10-02 08:57:05 -0300

    envio do site para o git

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Descrição do site para o google">
+    <meta name="keywords" content="palavras, para, o, meu site">
+    <title>Curso Danki</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Logomarca</div><!-- Logo -->
+        <nav class="desktop">
+            <ul>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>Sobre</a></li>
+                <li><a href=''>Serviços</a></li>
+                <li><a href=''>Contato</a></li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>Sobre</a></li>
+                <li><a href=''>Serviços</a></li>
+                <li><a href=''>Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="banner-principal">
+        <form action="">
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="cadastrar!">
+        </form>
+    </section><!-- banner principal-->
+
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Nome do autor</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero lectus, malesuada vel leo a, pharetra consequat mi. Maecenas ut purus lorem. Nullam bibendum mauris eu mi tincidunt scelerisque ac nec massa. Donec eu elit nec ligula ultricies facilisis.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In libero lectus, malesuada vel leo a, pharetra consequat mi. Maecenas ut purus lorem. Nullam bibendum mauris eu mi tincidunt scelerisque ac nec massa. Donec eu elit nec ligula ultricies facilisis.</p>
+        </div><!-- w50 -->
+        <div class="w50">
+            <img src="images/foto-autor.jpg" alt="">
+        </div>
+    </section><!-- descrição-autor -->
+
+</body>
+</html>
